3c63086337aa16cba4f0b24db3dfb8c7ce531593
berglundlovisa <63313681+berglundlovisa@users.noreply.github.com>
2022-03-30 19:13:51 +0200

    lade till i model och main Mystate

---
+++ lib/homepage_view.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import 'views/productviews/edit_jplatta_view.dart';
+import 'views/productviews/edit_jtelefon_view.dart';
+import 'views/productviews/edit_paronklocka_view.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Päron AB'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(height: 30),
+            const Text(
+              ('Products'),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Container(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const EditJTelefonView()),
+                );
+              },
+              //JTelefon ska fungera att ändra lagersaldo för
+              child: const Card(
+                elevation: 5,
+                child: ListTile(
+                  leading: Icon(Icons.smartphone),
+                  title: Text('JTelefon'),
+                  subtitle: Text(
+                      'Total stock balance: 250 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
+                  trailing: Icon(Icons.edit),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const EditJPlattaView()),
+                );
+              },
+              child: const Card(
+                elevation: 5,
+                child: ListTile(
+                  leading: Icon(Icons.tablet),
+                  title: Text('JPlatta'),
+                  subtitle: Text(
+                      'Total stock balance: 330 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
+                  trailing: Icon(Icons.edit),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const EditParonklockaView()),
+                );
+              },
+              child: const Card(
+                elevation: 5,
+                child: ListTile(
+                  leading: Icon(Icons.watch),
+                  title: Text('Päronklocka'),
+                  subtitle: Text(
+                      'Total stock balance: 288 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
+                  trailing: Icon(Icons.edit),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Widget _productItem()
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'views/productviews/edit_jplatta_view.dart';
-import 'views/productviews/edit_jtelefon_view.dart';
-import 'views/productviews/edit_paronklocka_view.dart';
+import 'package:provider/provider.dart';
+import 'homepage_view.dart';
+import 'model.dart';
 
 Future<void> main() async {
+  var state = MyState();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => state,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,92 +29,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Päron AB'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(height: 30),
-            const Text(
-              ('Products'),
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Container(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const EditJTelefonView()),
-                );
-              },
-              //JTelefon ska fungera att ändra lagersaldo för
-              child: const Card(
-                elevation: 5,
-                child: ListTile(
-                  leading: Icon(Icons.smartphone),
-                  title: Text('JTelefon'),
-                  subtitle: Text(
-                      'Total stock balance: 250 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
-                  trailing: Icon(Icons.edit),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const EditJPlattaView()),
-                );
-              },
-              child: const Card(
-                elevation: 5,
-                child: ListTile(
-                  leading: Icon(Icons.tablet),
-                  title: Text('JPlatta'),
-                  subtitle: Text(
-                      'Total stock balance: 330 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
-                  trailing: Icon(Icons.edit),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const EditParonklockaView()),
-                );
-              },
-              child: const Card(
-                elevation: 5,
-                child: ListTile(
-                  leading: Icon(Icons.watch),
-                  title: Text('Päronklocka'),
-                  subtitle: Text(
-                      'Total stock balance: 288 000'), //FÅ LAGERSALDO FRÅN FIRESTORE
-                  trailing: Icon(Icons.edit),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
